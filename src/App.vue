589b12b796a78e4832a6c63744df8c8450551304
nekoi <nekoi275@gmail.com>
2024-01-17 17:42:54 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
+import Header from '@/components/Header.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <Header class="text-base"></Header>
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          50: '#98a565',
+          100: '#414d08'
+        },
+        white: {
+          50: '#dae2e2'
+        },
+        purple: {
+          50: '#665a6a',
+          100: '#402e37'
+        }
+      },
+      keyframes: {
+        changeColor: {
+          from: { filter: 'hue-rotate(0deg)' },
+          to: { filter: 'hue-rotate(-360deg)' },
+        }
+      },
+      animation: {
+        'change-color': 'changeColor 5s infinite linear',
+      }
+    },
   },
   plugins: [],
 }
