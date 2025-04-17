1ba9667e52874966ec4026ba099da3201e73bada
nekoi <nekoi275@gmail.com>
2025-04-17 13:28:38 +0300

    teapot changes

--- src/components/Teapot.vue
+++ src/components/Teapot.vue
@@ -3,36 +3,32 @@
 <template>
   <div
     id="teapot-body"
-    class="bg-amber-950 w-[565px] h-[320px] rounded-full relative shadow-xl shadow-black cursor-pointer"
+    class="bg-amber-950 w-[565px] h-[320px] rounded-full relative shadow-xl shadow-black"
   >
     <h3
-      class="text-[100pt] bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent animate-change-color absolute left-[38%] top-[18%] block"
+      class="text-[750%] bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent animate-change-color absolute left-[39%] top-[18%] block"
     >
       福
     </h3>
     <div
       id="teapot-handle"
-      class="border-[40px] border-amber-950 w-[200px] h-[200px] rounded-full absolute top-[55px] right-[-95px]"
+      class="border-[40px] border-amber-950 w-[40%] h-[60%] rounded-full absolute top-[15%] right-[-17%]"
     ></div>
     <div
       id="teapot-nose-1"
-      class="bg-amber-950 w-[77px] h-[102px] rounded-full absolute top-[50px] left-[-17px] shadow-xl shadow-black"
+      class="bg-amber-950 w-[15%] h-[33%] rounded-full absolute top-[21%] left-[-4%] shadow-xl shadow-black"
     ></div>
     <div
       id="teapot-nose-2"
-      class="bg-amber-950 rounded-[15px] w-[76px] h-[76px] absolute top-[36px] left-[-42px] rotate-[50deg]"
-    ></div>
-    <div
-      id="teapot-nose-3"
-      class="border-t-[80px] border-amber-950 w-[150px] h-[150px] rounded-full absolute top-[19px] left-[-102px] rotate-[30deg]"
+      class="border-t-[80px] border-amber-950 w-[25%] h-[45%] rounded-full absolute top-[10%] left-[-15%] rotate-[30deg]"
     ></div>
     <div
       id="teapot-top"
-      class="bg-amber-950 w-[280px] h-[36px] rounded-full absolute top-[-34px] left-[135px] shadow-xl shadow-black"
+      class="bg-amber-950 w-[53%] h-[11%] rounded-full absolute top-[-10%] left-[24%] shadow-xl shadow-black"
     ></div>
     <div
       id="teapot-top-round"
-      class="bg-amber-950 w-[50px] h-[50px] rounded-full absolute top-[-75px] left-[250px]"
+      class="bg-amber-950 w-[11%] h-[17%] rounded-full absolute top-[-25%] left-[45%]"
     ></div>
   </div>
 </template>
