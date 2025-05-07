aeb99aaccb84375aa41bc310286b8efcb4df6206
nekoi <nekoi275@gmail.com>
2025-05-07 18:20:49 +0300

    changed assets names, added new project

--- src/components/Facts.vue
+++ src/components/Facts.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Flipcard from '@/components/FlipCard.vue';
 import type { cardInfo } from '@/components/FlipCard.vue';
-import fact1 from '@/assets/fact_1.gif';
-import fact2 from '@/assets/fact_2.webp';
-import fact3 from '@/assets/fact_3.webp';
+import factCv from '@/assets/fact_cv.gif';
+import factDrawings from '@/assets/fact_drawings.webp';
+import factPhotos from '@/assets/fact_photos.webp';
 
 const cards: cardInfo[] = [
   {
@@ -11,21 +11,21 @@ const cards: cardInfo[] = [
     data: 'Exploring the world of code with passion! Check out my CV — I’m eager to learn, grow, and contribute to your team with fresh ideas and enthusiasm.',
     linkName: 'Google doc CV',
     link: 'https://docs.google.com/document/d/1xcALkYF9U2flC6naGoAQtBuERRoRbpLxqMtDjlvRV4g/edit?tab=t.0#heading=h.m96rc670hd9p',
-    img: fact1
+    img: factCv
   },
   {
     heading: 'My drawings',
     data: 'Soft strokes, serene colors—where cats nap, birds perch, and flowers sway in quiet harmony. A touch of Zen in every sketch.',
     linkName: 'Doodle addicts',
     link: 'https://www.doodleaddicts.com/nekoi/',
-    img: fact2
+    img: factDrawings
   },
   {
     heading: 'My photography',
     data: 'Need high-quality, free photos? Browse my gallery and download stunning shots which may becomes great visuals for your projects!',
     linkName: 'Pexels',
     link: 'https://www.pexels.com/ru-ru/@2148646707/gallery/',
-    img: fact3
+    img: factPhotos
   },
   {
     heading: 'Pixel art drawn purely with CSS: 1px x 1px div + box-shadow property.',
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,52 +1,56 @@
 <script setup lang="ts">
 import Project from "@/components/Project.vue";
-import projectsBg from '@/assets/projects_bg.webp';
-import project1 from '@/assets/project_1.webp';
-import project2 from '@/assets/project_2.webp';
-import project3 from '@/assets/project_3.webp';
-import project4 from '@/assets/project_4.webp';
-import project5 from '@/assets/project_5.webp';
-import project6 from '@/assets/project_6.webp';
+import projectsBg from "@/assets/projects_bg.webp";
+import projectAr from "@/assets/project_ar.webp";
+import projectLayout from "@/assets/project_layout.webp";
+import projectWardrobe from "@/assets/project_wardrobe.webp";
+import projectTgschedule from "@/assets/project_tgschedule.webp";
+import projectBothelp from "@/assets/project_bothelp.webp";
+import projectCv1 from "@/assets/project_cv1.webp";
+import projectPokemon from "@/assets/project_pokemon.webp";
 
 const projects = [
   {
-    href: "https://nekoi275.github.io/aframe-test/",
-    img: project1,
+    href: "https://pokemon-tcg.pages.dev",
+    img: projectPokemon,
+    text: "Demo website based on Pokemon TCG API. Made with Vue 3.",
+  },
+  {
+    href: "https://nekoi275.github.io/aframe-test",
+    img: projectAr,
     text: "Web VR website made with Aframe framework.",
   },
   {
     href: "https://nekoi275.github.io/nice-shop-layout/#",
-    img: project2,
+    img: projectLayout,
     text: "E-commerce website layout sample.",
   },
   {
     href: "https://wardrobe.pages.dev/clothes",
-    img: project3,
+    img: projectWardrobe,
     text: "Personal wardrobe manager app with ability to add/edit/delete your clothes and accessories with photos and descriptions. Made with Vue 3. Test user credentials: test / test12345.",
   },
   {
-    href: "https://telegram-schedule.pages.dev/login",
-    img: project4,
-    text: "Telegram posts scheduler for AI generated images public group. Allows to set / edit / preview posts before posting into Telegram. Made with Vue 3.",
-  },
-  {
     href: "https://t.me/ai_inspire_test_bot",
-    img: project5,
+    img: projectBothelp,
     text: "Telegram bot help and description webapp. Made with Vue 3.",
   },
   {
-    href: "https://nekoi275.github.io/",
-    img: project6,
+    href: "https://nekoi275.github.io",
+    img: projectCv1,
     text: "Previous version of my website. Made with Vue 3.",
+  },
+  {
+    href: "https://telegram-schedule.pages.dev/login",
+    img: projectTgschedule,
+    text: "Telegram posts scheduler for AI generated images public group. Allows to set / edit / preview posts before posting into Telegram. Made with Vue 3.",
   },
 ];
 </script>
 
 <template>
   <section id="projects" class="bg-purple-100 p-6 relative">
-    <div
-      class="masonry sm:masonry-sm md:masonry-md z-20 relative"
-    >
+    <div class="masonry sm:masonry-sm md:masonry-md z-20 relative">
       <Project
         class="p-4 break-inside"
         v-for="project in projects"
@@ -54,7 +58,10 @@ const projects = [
         :key="project.href"
       ></Project>
     </div>
-    <div :style="{ backgroundImage: `url(${projectsBg})` }" class="absolute top-0 left-0 w-full h-full bg-cover opacity-25 z-0"></div>
+    <div
+      :style="{ backgroundImage: `url(${projectsBg})` }"
+      class="absolute top-0 left-0 w-full h-full bg-cover opacity-25 z-0"
+    ></div>
   </section>
 </template>
 
