77619a2a2ce88eddf657a3f6e6a2173d2cf4f313
nekoi <nekoi275@gmail.com>
2025-05-07 18:24:59 +0300

    minor footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+
+const currentYear = computed(() => new Date().getFullYear());
+</script>
 
 <template>
   <footer class="w-full bg-purple-50 h-14 text-center text-dark-50 pt-3 pb-3 text-xs">
-    <span class="text-green-50">Built with: </span>Vue 3, Tailwind CSS <span class="text-green-50">{{ new Date().getFullYear() }}</span>
+    <span class="text-green-600">Built with: </span>Vue 3, Tailwind CSS - <span class="text-green-600">{{ currentYear }}</span>
   </footer>
 </template>
 
